9909d1186cc82b6d1c471ff1a5433e1473b8cc4e
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2022-12-08 21:30:47 -0500

    correccion

--- apiServerwego/settings.py
+++ apiServerwego/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'apiServerwego.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templetes'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
